314d5a661c99d9fe1493c8c8e25bc96976a0b850
Прозоренко Константин Владиславович <ip314s18@csc.sibsutis.ru>
2024-02-25 15:16:34 +0700

    Added some error handles

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
 #define M_PI 3.14159265358979323846
 typedef struct
 {
@@ -10,15 +13,83 @@ typedef struct
     int number;
 } Circle;
 
-void parseCircle(char* buff,Circle* circle)
+bool parseCircle(char* buff,Circle* circle)
 {
     static int n = 0;
-    sscanf(buff,"%[^(](%f %f, %f)",circle->name,&circle->x,&circle->y,&circle->r);
+    int args=sscanf(buff,"%[^(](%f %f, %f)",circle->name,&circle->x,&circle->y,&circle->r)-1;
     circle->number = ++n;
+    if (args<1)
+    {
+        printf("Error: figure doesn't read\n\n");
+        return NULL;
+    }
+    if (circle->r<0)
+    {
+        printf("Error: incorrect radius\n\n");
+        return NULL;
+    }
+    if (args!=3)
+    {
+        printf("Error: count of args must be 3\n\n");
+        return NULL;
+    }
+    if (strcmp(circle->name,"circle")!=0)
+    {
+        printf("Error: expected 'circle'\n\n");
+        return NULL;
+    }
+    return 1;
+}
+bool parse(char* buff)
+{
+    if(strlen(buff) <= 1)
+    {
+        printf("Error: empty input\n");
+        return NULL;
+    }
+    char* first_bracket = strchr(buff, '(');
+    if(first_bracket == NULL)
+    {
+        printf("Error: expected '('\n");
+        return NULL;
+    }
+    char* second_bracket = strrchr(buff, ')');
+    if(second_bracket == NULL)
+    {
+        printf("Error: expected ')'\n");
+        return NULL;
+    }
+    if(strlen(second_bracket) > 2)
+    {
+        printf("Error: unexpected token\n");
+        return NULL;
+    }
+    int brackets_count = 1;
+    for(int i = 1; i < strlen(first_bracket); i++)
+    {
+        if(first_bracket[i] == '(')
+        {
+            brackets_count += 1;
+        } else if(first_bracket[i] == ')') 
+        {
+            brackets_count -= 1;
+        }
+    }
+
+    if(brackets_count > 0)
+    {
+        printf("Error: brackets are not closed\n");
+        return NULL;
+    }
+    return 1;
 }
 void printCircle(Circle* circle)
 {
     printf("%d. %s(%.f %.f, %.1f)\n",circle->number,circle->name,circle->x, circle->y, circle->r);
+    float perimetr = 2*M_PI*circle->r;
+    float area = M_PI*pow(circle->r,2);
+    printf("perimeter = %.4f\n", perimetr);
+    printf("area = %.4f\n\n",area);
 } 
 int main()
 {
@@ -28,55 +99,37 @@ int main()
     geometry = fopen("main.txt","r");
     if (geometry == NULL)
     {
-        printf("The file doesn't open");
+        printf("The file doesn't open\n");
         return 1;
     }
     
     char buff[256];
-    while((fgets(buff,256,geometry))!=NULL)
+   
+    
+    if (fgets(buff, 256, geometry) == NULL)
     {
-        parseCircle(buff,&circle);
-        printCircle(&circle);
+        printf("The file is empty\n");
+        fclose(geometry);
+        return 1;
     }
+    else
+    {
+        do
+        {
+            if(parseCircle(buff,&circle) && parse(buff))
+            {
+                printCircle(&circle);
+            }
+            else 
+            {
+                printf("in circle %d",circle.number);
+                fclose(geometry);
+                return 1;
+            }
+        } while(fgets(buff, 256, geometry) != NULL);
+    }
+    
     fclose(geometry);
-    // while((fgets(buff,256,geometry))!=NULL)
-    // {
-    //     char name[20];
-    //     sscanf(buff,"%[^(]", name);
-    //     if (strcmp(name,"circle")!=0 || strcmp(name,"triangle")!=0)
-    //     {
-    //         printf("\nError: expected 'circle' or 'triangle'");
-    //         return 1;
-    //     }
-    //     char* first_bracket = strchr(buff, '(');
-    //     if(first_bracket == NULL)
-    //     {
-    //         puts("expected '('");
-    //         return 1;
-    //     }
-    //     if (strcmp(name,"circle")==0)
-    //     {
-    //         numberShape++;
-    //         float x,y,r;
-    //         sscanf(buff, "circle(%f %f, %f)", &x, &y, &r);
-    //         float perimetr = 2*M_PI*r;
-    //         float area = M_PI*pow(r,2);
-    //         printf("%d. %s(%.f %.f, %.1f)\n",numberShape,name,x,y,r);
-    //         printf("perimeter = %.4f\n", perimetr);
-    //         printf("area = %.4f\n\n",area);
-    //     }
-    //     if (strcmp(name,"triangle")==0)
-    //     {
-    //         numberShape++;
-    //         float x1,y1,x2,y2,x3,y3;
-    //         sscanf(buff, "triangle((%f %f, %f %f, %f %f))", &x1, &y1, &x2,&y2,&x3,&y3);
-    //         float perimetr = sqrt(pow(x2-x1,2)+pow(y2-y1,2))+sqrt(pow(x3-x2,2)+pow(y3-y2,2))+sqrt(pow(x3-x1,2)+pow(y3-y1,2));
-    //         float area = 0.5*fabs((x2-x1)*(y3-y1)-(x3-x1)*(y2-y1));
-    //         printf("%d. %s((%.f %.f, %.f %.f, %.f %.f))\n",numberShape,name,x1, y1, x2, y2, x3, y3);
-    //         printf("perimeter = %.3f\n", perimetr);
-    //         printf("area = %.f\n\n",area);  
-    //     }
-    // }
     
     return 0;
 }
