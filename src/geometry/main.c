b96036501c16ca0a04a59fc17b0e626fb49df4c6
Прозоренко Константин Владиславович <ip314s18@csc.sibsutis.ru>
2024-03-29 23:16:19 +0700

    added main.c

---
+++ src/geometry/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include <libgeometry/parser.h>
+int main()
+{
+    FILE *file = NULL;
+    file = fopen("main.txt","r");
+    if (file == NULL)
+    {
+        printf("The file doesn't open\n");
+        return 1;
+    }
+    else 
+    {
+        parser(file);
+        fclose(file);
+    }
+    return 0;
+}
