babc1b5aad12d1e3be325df763077becd538fbe6
Прозоренко Константин Владиславович <ip314s18@csc.sibsutis.ru>
2024-04-07 19:15:10 +0700

    added lexer tests

---
+++ test/tests.c
@@ -0,0 +1,49 @@
+#include <ctest.h>
+#include <libgeometry/calculate.h>
+#include <libgeometry/intersects.h>
+#include <libgeometry/lexer.h>
+#include <libgeometry/parser.h>
+#include <stdio.h>
+
+CTEST(lexer, to_lower_string)
+{
+    char string1[] = "Circle";
+    char string2[] = "TRIangle";
+    const char expected1[] = "circle";
+    const char expected2[] = "triangle";
+
+    to_lower_string(string1);
+    to_lower_string(string2);
+
+    ASSERT_STR(string1, expected1);
+    ASSERT_STR(string2, expected2);
+}
+
+CTEST(lexer, count_char)
+{
+    char target = 'q';
+    char string[] = "uq";
+    int expected = 1;
+    int result = count_char(string, target);
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(lexer, remove_extra_spaces)
+{
+    char string[] = "1   5";
+    char expected[] = "1 5";
+    char string2[] = "1   5 1    8";
+    char expected2[] = "1 5 1 8";
+    remove_extra_spaces(string);
+    remove_extra_spaces(string2);
+    ASSERT_STR(expected, string);
+    ASSERT_STR(expected2, string2);
+}
+
+CTEST(lexer, str_clear)
+{
+    char string[] = "123456789";
+    char expected[] = "";
+    str_clear(string, strlen(string));
+    ASSERT_STR(expected, string);
+}
